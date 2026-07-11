0945a519f2a2114f453001b4bd815e3805b446b3
saaaachi <f9mg56p26s@privaterelay.appleid.com>
2026-07-11 19:01:46 +0900

    Create works.js with details for various coloring pages
    Added a new array of works with details for each item.

---
+++ data/works.js
@@ -0,0 +1,91 @@
+const works = [
+
+{
+    id: "PL-000001",
+
+    title: "消防車ぬりえ",
+
+    description: "消防車のぬりえです。"
+
+    ,
+
+    category: [
+        "子ども向け"
+    ],
+
+    fixedTags: [
+        "ぬりえ"
+    ],
+
+    freeTags: [
+        "消防車",
+        "車"
+    ],
+
+    series: "はたらく車",
+
+    difficulty: 1,
+
+    age: "3歳〜",
+
+    paper: "A4",
+
+    tools: "クレヨン・色鉛筆",
+
+    thumbnail:
+    "assets/images/thumbnail/firetruck.jpg",
+
+    pdf:
+    "assets/pdf/firetruck.pdf",
+
+    recommend: true,
+
+    isNew: true,
+
+    date: "2026-07-11"
+},
+
+{
+    id: "PL-000002",
+
+    title: "パトカーぬりえ",
+
+    description: "パトカーのぬりえです。",
+
+    category: [
+        "子ども向け"
+    ],
+
+    fixedTags: [
+        "ぬりえ"
+    ],
+
+    freeTags: [
+        "パトカー",
+        "車"
+    ],
+
+    series: "はたらく車",
+
+    difficulty: 1,
+
+    age: "3歳〜",
+
+    paper: "A4",
+
+    tools: "クレヨン・色鉛筆",
+
+    thumbnail:
+    "assets/images/thumbnail/police.jpg",
+
+    pdf:
+    "assets/pdf/police.pdf",
+
+    recommend: true,
+
+    isNew: true,
+
+    date: "2026-07-11"
+}
+
+];
