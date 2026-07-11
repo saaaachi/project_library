84f15120a3d42d4f87eb3097b6a1fdd0af070210
saaaachi <f9mg56p26s@privaterelay.appleid.com>
2026-07-11 12:55:28 +0900

    Add works.js with initial data for projects

---
+++ assets/data/works.js
@@ -0,0 +1,46 @@
+const works = [
+
+{
+    id: 1,
+
+    title: "消防車ぬりえ①",
+
+    category: [
+        "子ども向け"
+    ],
+
+    fixedTags: [
+        "ぬりえ"
+    ],
+
+    freeTags: [
+        "車",
+        "消防車",
+        "働く車"
+    ],
+
+    level: 1,
+
+    age: "3〜6歳",
+
+    size: "A4",
+
+    tools: [
+        "色えんぴつ"
+    ],
+
+    description:
+        "消防車のぬりえです。",
+
+    thumbnail:
+        "assets/images/sample.jpg",
+
+    pdf:
+        "assets/pdf/firetruck01.pdf",
+
+    isNew: true,
+
+    recommend: true
+}
+
+];
